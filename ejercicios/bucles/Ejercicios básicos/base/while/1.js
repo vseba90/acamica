8bab446c5e8e2e37194eb460ce977950e0d727b0
Sebastian Villanueva <villanueva.sebastian90@gmail.com>
2018-08-12 14:47:09 -0300

    Se agrego ejercicio while

--- ejercicios/bucles/Ejercicios básicos/base/while/1.js
+++ ejercicios/bucles/Ejercicios básicos/base/while/1.js
@@ -1,3 +1,10 @@
 console.log('Ciclo While - Ejercicio 1');
 
-var numeroUsuario = parseInt(prompt('Ingresa un numero papa'));
+var numeroUsuario;
+
+while(numeroUsuario !== 0){
+    var numeroUsuario = parseInt(prompt('Ingresa un numero pap'));
+    if(numeroUsuario === 0){
+        alert('Este es el numero que esperaba');
+    }
+}
