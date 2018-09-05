c6e6dcac75aa155a7791c8ff90eb6d7f94481a40
Sebastian Villanueva <villanueva.sebastian90@gmail.com>
2018-09-05 14:40:01 -0300

    Loteria

--- ejercicios/bucles/Para curiosos/base/no-vimos/1.js
+++ ejercicios/bucles/Para curiosos/base/no-vimos/1.js
@@ -2,23 +2,27 @@ console.log('No vimos - Ejercicio 1');
 
 window.onload = () => {
     gsheets.getWorksheet('1i5g7So0PAzZ1hdGTbajOOpahzirQxkgoBLWdxarq9x8', 'cartones')
-    .then(cartones => cartones.data)
-    .then(cartones => cartones.map(Object.values))
-    .then(cartones => {
-        dibujarCartones(cartones);
-    })
-    .catch(err => console.error(err));
+        .then(cartones => cartones.data)
+        .then(cartones => cartones.map(Object.values))
+        .then(cartones => {
+            dibujarCartones(cartones);
+        })
+        .catch(err => console.error(err));
 };
 
-document.body.appendChild(carton);
-var carton = document.createElement('div');
-carton.classList.add('carton');
-var cuadrados = document.createElement('div');
-cuadrados.classList.add('cuadrados');
+
 
 function dibujarCartones(cartones) {
-    for(var i = 0; i < cartones.lenght; i++){
-        carton.appendChild(cuadrados);
+    for (var i = 0; i < cartones.length; i++) {
+        const div = document.createElement('div').classList.add('carton');
+        const container = document.createElement('div').classList.add('carton-container');
+        document.body.appendChild(div);
+        div.appendChild(container);
+        for (var j = 0; j < cartones[i].length; j++) {
+            const divItinerate = document.createElement('div').classList.add('cuadrados');
+            container.appendChild(divItinerate);   
+            divItinerate.innerHTML(cartones[i][j]);
+        }
     }
 }
 
